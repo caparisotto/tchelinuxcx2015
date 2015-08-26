5c0d7f0cc49e336ef49432774591a692f01adc28
caparisotto <carlos.parisotto@hotmail.com>
2015-08-26 09:44:11 -0300

    Change 260803

--- palestra/settings.py
+++ palestra/settings.py
@@ -57,11 +57,13 @@ ROOT_URLCONF = 'palestra.urls'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tchelinux',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+        #'NAME': 'tchelinux',                      # Or path to database file if using sqlite3.
+        #'USER': 'postgres',                      # Not used with sqlite3.
+        #'PASSWORD': '',                  # Not used with sqlite3.
+        #'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+        #'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+	'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
